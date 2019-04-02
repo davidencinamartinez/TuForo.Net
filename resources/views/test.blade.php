a02f0db9bab197bf2d81f5ac7b436da0d0da4132
Señor Alcalde <davidencina1996@gmail.com>
2019-04-02 20:27:03 +0200

    02042019_Commit_5

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,12 +3,25 @@
 @section('title', ' - Test')
 
 @push('styles')
-	<link rel='stylesheet' type='text/css' href='{{ asset("css/posts.css") }}'>
+	<link rel='stylesheet' type='text/css' href='{{ asset("css/subject.css") }}'>
 @endpush
 @section('postSection')
-<br>
-    <div id="con">
-    	lalalala<br>
-    	JUJUJUJU<br>
+    <div id='subjectPanel'>
+    	<div id='subject'>
+    		<table>
+    			<tr>
+    				<td>
+    					<img id='subjectImg' src='https://cdn.pixabay.com/photo/2012/04/12/23/47/car-30984_960_720.png'>
+    					<label>Bienvenido a TuForo.Net</label>
+    				</td>
+    				<td>
+    					<label><?php echo date('d-m-Y H:m:s'); ?></label>
+    				</td>
+    				<td>
+    					PEDRO
+    				</td>
+    			</tr>	
+    		</table>
+    	</div>
     </div>
 @stop
