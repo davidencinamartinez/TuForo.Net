2b1658a41afcb31420104dd145082cb65c9102cc
David Encina <dencina1996@gmail.com>
2019-11-22 13:10:49 +0100

    22112019_1310

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,23 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Jenssegers\Agent\Agent;
 use DB;
 
 class RegisterController extends Controller
 {
     public function create() {
 
-        return view('register');
+    	$agent = new Agent(); // DEVICE INFO
+		
+		if ($agent->isMobile()) {
+        	return view('mobile.register');
+        } else {
+        	return view('register');
+        }
 
     }
-
-    public function index() {
-      $data = DB::select('select username from users');
-      
-      
-      //return response()->json(['data'=>$data]);
-      return view('lol', ['mydata' => $data]);
-
-
-   }
 }
---
+++ resources/views/mobile/register.blade.php
@@ -0,0 +1,36 @@
+@extends('mobile.main')
+
+@section('title', 'Registro - TuForo.Net')
+
+@push('styles')
+@endpush
+@push('scripts')
+	<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+@endpush
+@section('postSection')
+	<div id="registerPanel">
+	    <div id="inputForm">
+	        <form>
+	            <p><b>Nombre de Usuario</b>*</p><input type="text" name="reg_username" maxlength="20" autofocus>
+	            <p><b>Correo electrónico</b>*</p><input type="text" name="reg_email" maxlength="64">
+	            <p><b>Contraseña</b>*</p><input type="password" name="reg_password" maxlength="64">
+	            &emsp;
+	            <img show='on' src="storage/src/other/show.png" onclick="showPassword(this)">
+	            <p><b>Confirmar Contraseña</b>*</p><input type="password" name="reg_passwordVerified" maxlength="64">
+	            &emsp;
+	            <img show='on' src="storage/src/other/show.png" onclick="showPassword(this)">
+	            <p>* (Todos los campos son obligatorios)</p>
+	            <div class="g-recaptcha" data-sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI" style="margin: 20px 0px 20px 0px;"></div>
+	            <p>
+	                <input type="checkbox" name="reg_terms" style="vertical-align: bottom;">
+	                <label>He leído y acepto los términos y condiciones de </label>
+	                <b>TuForo.Net</b>
+	            </p>
+	            <input type="button" id="reg_done" value="Registrarse" onclick="return formValidation()">
+	        </form>
+	    </div>
+	    <div id="privacyPolicy">
+	        <iframe src="storage/src/other/AvisoLegal.pdf" type="application/pdf" width="100%" height="100%"></iframe>
+	    </div>
+	</div>
+@stop
