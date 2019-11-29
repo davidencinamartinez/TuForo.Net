821d5b49e85d2e65f1962958abf222a83d6db4a6
David Encina <dencina1996@gmail.com>
2019-11-29 11:40:06 +0100

    29112019_1140

--- public/js/register.js
+++ public/js/register.js
@@ -15,118 +15,8 @@ function showPassword(element) {
 	}
 }
 
-/*function userAvailable() {
-	var user = $('input[name="reg_username"]').val();
-	var _token = $('meta[name="csrf-token"]').attr('content');
-	$.ajax({
-	    url: '/ajax/checkUser',
-	    type: 'POST',
-	    data: { user: user, _token: _token },
-	    success: function (result) {
-			getIt(result);
-	        
-	    },
-	    complete: function(x) {
-	    	console.log(this.url);
-	    	console.log(x);
-	    }
-
-	});
-};
-
-
-function mailAvailable() {
-	var mail = $('input[name="reg_email"]').val();
-	var _token = $('input[name="_token"]').val();
-	$.ajax({
-	    url: '/ajax/checkMail',
-	    type: 'POST',
-	    datatype: 'json',
-	    data: { mail: mail, _token: _token },
-	    success: function (result) {
-	        if (result == 'true') {
-	            $('input[name="reg_email"]').before().remove('.error');
-	            errorDisplay($('input[name="reg_email"]'),'El correo introducido ya está en uso');
-	        } else {
-	            $('input[name="reg_email"]').before().remove('.error');
-	        }
-	    }
-	});
-}
-
-function checkName() {
-	var user = $('input[name="reg_username"]');
-	if (user.val().length < 6) {
-		errorDisplay(user,'El nombre de usuario debe contener mínimo 6 carácteres');
-		return false;
-	} else if (user.val().length > 20) {
-		errorDisplay(user,'El nombre de usuario debe contener máximo 20 carácteres');
-		return false;
-	} else {
-		return true;
-	}
-}
-
-function checkMail() {
-	var mail = $('input[name="reg_email"]');
-	var reg = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,3}\b$/i;
-	if (reg.test(mail.val())) {
-        return true;
-    } else {
-        errorDisplay(mail,'El correo electrónico introducido no es correcto');
-        return false;
-    }
-}
-
-function checkPassword() {
-	var pass = $('input[name="reg_password"]');
-	var pass_v = $('input[name="reg_passwordVerified"]');
-	if (pass.val().length < 8) {
-		errorDisplay(pass,'La contraseña introducida es demasiado corta');
-		return false;
-	} else if (pass.val() !== pass_v.val()) {
-		errorDisplay(pass_v,'Las contraseñas no coinciden');
-		return false;
-	} else {
-		return true;
-	}
-}
-function checkCaptcha() {
-	var captcha = $('.g-recaptcha');
-	var captchaResponse = grecaptcha.getResponse();
-	if (captchaResponse.length <= 0) {
-		errorDisplay(captcha,'Verifica que eres humano');
-		return false;
-	} else {
-		return true;
-	}
-}
-
-function checkTerms() {
-	var terms = $('input[name="reg_terms"]');
-	if (terms.is(':checked') == false) {
-		errorDisplay(PTerms,'<br>Debes aceptar las condiciones de TuForo.Net');
-		return false;
-	} else {
-		return true;
-	}
-}
-*/
-function printErrorMsg (msg) {
-            $(".print-error-msg").find("ul").html('');
-            $(".print-error-msg").css('display','block');
-            $.each( msg, function( key, value ) {
-                $(".print-error-msg").find("ul").append('<li>'+value+'</li>');
-            });
-        }
-
 function formValidation() {
 	$('.error').remove();
-    /*
-    var checkForm = [checkName(),checkMail(),checkPassword(),checkCaptcha(),checkTerms()];
-    var result = 0;
-    var verifyMail = $('input[name="reg_email"]').val();
-    */
 	$.ajaxSetup({
 		headers: {
 			'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -148,6 +38,7 @@ function formValidation() {
         })
         .done(function(data) {
         	if($.isEmptyObject(data.error)){
+        		$('#registerPanel').empty();
 	        	var successPanel = $('<div></div>').attr('id', 'successPanel').css('background-color', 'value');
 	            var successImg = $('<img>').attr({
 	            	id: 'successImg',
@@ -155,7 +46,7 @@ function formValidation() {
 	            });
 	            var successText = 	'<b>Bienvenido a TuForo.Net</b>!'+
 	            					'<br />Disfruta de tu estancia!'+
-	            					'<a href="/"><h3>Haz click aquí para volver al inicio</h3></a>';
+	            					'<a id="successLink" href="/"><h3>Haz click aquí para volver al inicio</h3></a>';
 				successPanel.html('<img id="successImg" src="storage/src/other/reg_done.png"><br><br>'+successText);
 				$('#registerPanel').append(successPanel);
         	}else{
@@ -186,7 +77,4 @@ function formValidation() {
         	console.log('error');
 
         })
-        .always(function() {
-            
-        });       
 }
